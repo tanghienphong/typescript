177bedd59bb60657ac6a7fc82e389a1306e05036
Kelvin Zeng <tanghienphong@gmail.com>
2019-05-24 18:34:09 +0700

    update

---
+++ a.js
@@ -0,0 +1,3 @@
+var a = false;
+var arrNumer = [1, 2, 3, 4, 4, 5, 6, 7];
+console.log(arrNumer);
---
+++ a.ts
@@ -0,0 +1,3 @@
+let a:boolean = false;
+let arrNumer:number[] = [1,2,3,4,4,5,6,7] ;
+console.log(arrNumer);
